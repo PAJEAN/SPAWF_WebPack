263f2577bbe3319219185f6b2fb70833c39b69e9
PAJEAN <pierre-antoine.jean@mines-ales.fr>
2022-09-28 14:41:38 +0200

    Remove try/catch of pages.

--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -1,35 +1,30 @@
-export default (function () {
-    try {
-        const PAGE_NAME = 'about-contact';
+(function () {
+    const PAGE_NAME = 'about-contact';
 
-        const TEMPLATE = document.createElement('template');
-        TEMPLATE.innerHTML = /* html */`
+    const TEMPLATE = document.createElement('template');
+    TEMPLATE.innerHTML = /* html */`
 
-            <style></style>
+        <style></style>
 
-            <div id="main">
-                <wc-header></wc-header>
-                <div class="container">
-                    <h3>Une nouvelle page sans rechargement !</h3>
-                </div>
+        <div id="main">
+            <wc-header></wc-header>
+            <div class="container">
+                <h3>Une nouvelle page sans rechargement !</h3>
             </div>
+        </div>
 
-        `;
+    `;
 
-        window.customElements.define(PAGE_NAME, class extends HTMLElement {
-            constructor() {
-                super();
-            }
-        
-            connectedCallback () {
-                this.appendChild(TEMPLATE.content.cloneNode(true));
-                this.content = this.querySelector('#main');
-            }
-        
-            disconnectedCallback () {}
-        });
-    }
-    catch (err) {
-        console.error(err);
-    }
+    window.customElements.define(PAGE_NAME, class extends HTMLElement {
+        constructor() {
+            super();
+        }
+    
+        connectedCallback () {
+            this.appendChild(TEMPLATE.content.cloneNode(true));
+            this.content = this.querySelector('#main');
+        }
+    
+        disconnectedCallback () {}
+    });
 })();
